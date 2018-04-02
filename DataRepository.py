30e242ddaa4f58ad9d42d731cdaa5c7e8a36a3e1
Cheema,Nimrat <nm-ch@Hotmail.com>
2018-04-02 11:26:23 -0400

    Make sparse matrix from book data
    matrix is bookid vs user id with rating as values

--- DataRepository.py
+++ DataRepository.py
@@ -10,42 +10,59 @@ class DataRepository():
 
     def __init__(self,user_start_id):
         self.user_start_id=user_start_id
+    def get_users(self):
+        return list(self.db['user'].find())
 
     def scrape_books(self):
         gc = client.GoodreadsClient(os.environ['goodreads_key'],os.environ['goodreads_secret'])
         gc.authenticate(os.environ["access_token"], os.environ["access_secret"])
-        user_start=gc.user(self.user_start_id)
-        user_friends=user_start.friends()
-        for u_f in user_friends:
-            goodread_user=GoodreadsUser(u_f,gc)
-            mongo_user=self.db['user'].find_one({'gid':u_f["id"]})
-            if mongo_user is None and int(u_f['reviews_count'])>5:
-                mongo_user={"gid":u_f["id"],
-                      "name":u_f["name"],
-                      "reviews_count":u_f["reviews_count"],
-                      "image_url":u_f["image_url"]
-                }
-                user_id=self.db['user'].insert_one(mongo_user).inserted_id
-                pages = math.floor(int(u_f['reviews_count']) / 20)+2
-                for i in range(1,pages):
-                    reviews=goodread_user.reviews(page=i)
-                    for review in reviews:
-                        mongo_book=self.db['books'].find_one({'id': review['book']['id']["#text"]})
-                        if mongo_book is None:
-                            mongo_book=dict(review['book'])
-                            mongo_book['id']=mongo_book['id']["#text"]
-                            mongo_book['text_reviews_count'] = mongo_book['text_reviews_count']["#text"]
-                            mongo_book['work'] = mongo_book['work']["id"]
-                            mongo_book_id=self.db['books'].insert_one(mongo_book).inserted_id
-                            mongo_book["_id"]=mongo_book_id
-                        mongo_book_id=mongo_book["_id"]
-                        if int(review['rating'])>0:
-                            mongo_review={"book_id":mongo_book_id,"rating":int(review['rating'])}
-                            self.db['user'].update_one(
-                                {"_id":user_id},
-                                {
-                                    "$push":{"reviews":mongo_review}
-                                })
+        start_id=self.user_start_id
+
+        open_list = []
+        while(True):
+            user_start = gc.user(start_id)
+            user_friends = user_start.friends()
+            for u_f in user_friends:
+                open_list.append(u_f["id"])
+                goodread_user=GoodreadsUser(u_f,gc)
+                mongo_user=self.db['user'].find_one({'gid':u_f["id"]})
+                if mongo_user is None and int(u_f['reviews_count'])>5:
+
+                    mongo_user={"gid":u_f["id"],
+                          "name":u_f["name"],
+                          "reviews_count":u_f["reviews_count"],
+                          "image_url":u_f["image_url"]
+                    }
+                    user_id=self.db['user'].insert_one(mongo_user).inserted_id
+                    pages = math.floor(int(u_f['reviews_count']) / 20)+2
+                    for i in range(1,pages):
+                        add_reviews=True
+                        try:
+                            reviews=goodread_user.reviews(page=i)
+                        except:
+                            add_reviews=False
+                        if add_reviews:
+                            for review in reviews:
+                                try:
+                                    mongo_book=self.db['books'].find_one({'id': review['book']['id']["#text"]})
+                                    if mongo_book is None:
+                                        mongo_book=dict(review['book'])
+                                        mongo_book['id']=mongo_book['id']["#text"]
+                                        mongo_book['text_reviews_count'] = mongo_book['text_reviews_count']["#text"]
+                                        mongo_book['work'] = mongo_book['work']["id"]
+                                        mongo_book_id=self.db['books'].insert_one(mongo_book).inserted_id
+                                        mongo_book["_id"]=mongo_book_id
+                                    mongo_book_id=mongo_book["_id"]
+                                    if int(review['rating'])>0:
+                                        mongo_review={"book_id":mongo_book_id,"rating":int(review['rating'])}
+                                        self.db['user'].update_one(
+                                            {"_id":user_id},
+                                            {
+                                                "$push":{"reviews":mongo_review}
+                                            })
+                                except:
+                                    pass
+            start_id=open_list.pop(0)
 
 
 
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 os.environ["goodreads_key"]='b3ULykyky8T07LAcKdlXQ'
 os.environ["goodreads_secret"]='dUcc6K7WEos3Vyq2A2JkYX1iHMCuR8UK0cCFvZK1kc'
 
@@ -6,5 +7,17 @@ os.environ["access_token"]='kLeEvIkRdHHTBwQbB4U5mg'
 os.environ["access_secret"]='EWBJSKBwlGJ59pLWgT8FRUCYzGyjSE1WYGgvhi4'
 
 from DataRepository import DataRepository
-dr=DataRepository(17076701)
-dr.scrape_books()
+dr=DataRepository(9616934)
+users=dr.get_users()
+flat_users=[]
+for user in users:
+    if 'reviews' in user:
+        for review in user['reviews']:
+            flat_users.append({
+                "user_id":user["_id"],
+                "book_id":review["book_id"],
+                "rating":review["rating"]
+            })
+df=pd.DataFrame(flat_users)
+df_matrix=df.pivot('book_id','user_id','rating').fillna(0)
+i=0;
